9133e634101080dd4340ae0684275fec8267b4dc
Howard M. Lewis Ship <hlship@gmail.com>
2015-04-03 09:17:39 -0700

    Strip down to first step in tutorial

--- src/main/java/com/example/tutorial/components/Layout.java
+++ src/main/java/com/example/tutorial/components/Layout.java
@@ -2,12 +2,10 @@ package com.example.tutorial.components;
 
 import org.apache.tapestry5.BindingConstants;
 import org.apache.tapestry5.ComponentResources;
-import org.apache.tapestry5.SymbolConstants;
 import org.apache.tapestry5.annotations.Import;
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.ioc.annotations.Inject;
-import org.apache.tapestry5.ioc.annotations.Symbol;
 
 /**
  * Layout component for pages of application test-project.
@@ -27,24 +25,4 @@ public class Layout
 
 	@Property
 	private String pageName;
-
-	@Property
-	@Inject
-	@Symbol(SymbolConstants.APPLICATION_VERSION)
-	private String appVersion;
-
-
-
-	public String getClassForPageName()
-	{
-		return resources.getPageName().equalsIgnoreCase(pageName)
-				? "active"
-				: null;
-	}
-
-	public String[] getPageNames()
-	{
-		return new String[]{"Index", "About", "Contact"};
-	}
-
 }
